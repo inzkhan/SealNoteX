448d9b88c6ddc40ab4ed527fce19c15920d42ce0
InziKhan <inzkhan@gmail.com>
2019-07-21 14:32:44 +0500

    Changes note entity and also added code in TagName class.

--- app/src/main/java/com/inzi/sealnotex/roomdatabase/TagId.java
+++ app/src/main/java/com/inzi/sealnotex/roomdatabase/TagId.java
@@ -2,10 +2,29 @@ package com.inzi.sealnotex.roomdatabase;
 
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "note_tag",foreignKeys ={ @ForeignKey(entity = Note.class,parentColumns = "note_id",childColumns = "id"),
+        @ForeignKey(entity = TagName.class,parentColumns = "tag_id",childColumns = "id")})
 public class TagId {
 
-    private int note_id;
-    private int tag_id;
+    @PrimaryKey
+    private int note_id,tag_id;
+
+    public int getNote_id() {
+        return note_id;
+    }
+
+    public void setNote_id(int note_id) {
+        this.note_id = note_id;
+    }
+
+    public int getTag_id() {
+        return tag_id;
+    }
+
+    public void setTag_id(int tag_id) {
+        this.tag_id = tag_id;
+    }
 }
