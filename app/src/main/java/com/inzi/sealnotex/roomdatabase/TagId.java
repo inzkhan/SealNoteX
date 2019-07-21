c35a69d541a79540adb1b0ede999e57406eab612
InziKhan <inzkhan@gmail.com>
2019-07-21 14:09:21 +0500

    Changes note entity and also added code in TagName class.

--- app/src/main/java/com/inzi/sealnotex/roomdatabase/TagId.java
+++ app/src/main/java/com/inzi/sealnotex/roomdatabase/TagId.java
@@ -1,4 +1,11 @@
 package com.inzi.sealnotex.roomdatabase;
 
+
+import androidx.room.Entity;
+
+@Entity
 public class TagId {
+
+    private int note_id;
+    private int tag_id;
 }
--- app/src/main/java/com/inzi/sealnotex/roomdatabase/TagName.java
+++ app/src/main/java/com/inzi/sealnotex/roomdatabase/TagName.java
@@ -1,12 +1,30 @@
 package com.inzi.sealnotex.roomdatabase;
 
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "tags")
+@Entity(tableName = "tags", indices = {@Index(value = {"name"}, unique =
+ true)})
 public class TagName {
 
     @PrimaryKey(autoGenerate = true)
-    private String id;
+    private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
