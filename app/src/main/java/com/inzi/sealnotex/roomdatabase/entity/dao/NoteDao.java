61b28dd48c96a02a0b6651a01de574cde7ca9761
InziKhan <inzkhan@gmail.com>
2019-07-21 18:24:57 +0500

    New methods added in NatoDao

--- app/src/main/java/com/inzi/sealnotex/roomdatabase/entity/dao/NoteDao.java
+++ app/src/main/java/com/inzi/sealnotex/roomdatabase/entity/dao/NoteDao.java
@@ -2,7 +2,9 @@ package com.inzi.sealnotex.roomdatabase.entity.dao;
 
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import com.inzi.sealnotex.SealNote;
 
@@ -11,10 +13,48 @@ import java.util.List;
 @Dao
 public interface NoteDao {
 
+    @Update
+    void updateNote(SealNote sealNote);
 
-    //public void updateNote();
+    @Insert
+    void addNote(SealNote sealNote);
+
+    @Query("SELECT * FROM notes WHERE id =:note_id")
+    SealNote getNoteById(int note_id);
 
     @Query("SELECT * FROM notes")
-    public List<SealNote> getAllNotes();
+     List<SealNote> getAllNotes();
+
+    @Query("SELECT * FROM notes, note_tag " +
+            "    WHERE archived = '0' AND " +
+            "          deleted= '0' AND " +
+            "          tag_id =:tag_id AND " +
+            "          note_id = notes.Id " +
+            "     ORDER BY deleted DESC")
+     List<SealNote> getAllNotesByTagId(int tag_id);
+
+
+    @Query("SELECT * FROM notes" +
+            " WHERE archived = '1' AND " +
+             " deleted = '0'" +
+            " ORDER BY deleted DESC")
+    List<SealNote> getAllArchivedNotes();
+
+
+    @Query("SELECT * FROM notes " +
+            "WHERE deleted = '1' ORDER BY " +
+             "edited DESC")
+    List<SealNote> getAllDeletedNotes();
+
+
+    @Query("DELETE FROM notes WHERE id= :note_id")
+    void deleteNoteById(int note_id);
+
+
+    @Query("UPDATE notes SET deleted =:trash WHERE id=:note_id")
+    void trashNoteById(int note_id,int trash);
+
+    @Query("UPDATE notes SET archived =:archive WHERE id=:note_id")
+    void archiveNoteById(int note_id ,int archive);
 
 }
