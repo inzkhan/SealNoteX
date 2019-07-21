901db1a329a54440dacf380d399704bbeab4167d
InziKhan <inzkhan@gmail.com>
2019-07-21 07:04:05 +0500

    Updated Note entity class

--- app/src/main/java/com/inzi/sealnotex/roomdatabase/Note.java
+++ app/src/main/java/com/inzi/sealnotex/roomdatabase/Note.java
@@ -1,4 +1,110 @@
 package com.inzi.sealnotex.roomdatabase;
 
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note {
+
+    @PrimaryKey
+    private String Id;
+    private String position;
+    private String title;
+    private String content;
+    private String extra_content;
+    private String color;
+    private String created;
+    private String edited;
+    private String archived;
+    private String deleted;
+    private String type;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getExtra_content() {
+        return extra_content;
+    }
+
+    public void setExtra_content(String extra_content) {
+        this.extra_content = extra_content;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public String getEdited() {
+        return edited;
+    }
+
+    public void setEdited(String edited) {
+        this.edited = edited;
+    }
+
+    public String getArchived() {
+        return archived;
+    }
+
+    public void setArchived(String archived) {
+        this.archived = archived;
+    }
+
+    public String getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(String deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
